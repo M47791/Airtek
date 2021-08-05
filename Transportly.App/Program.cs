7db611167ce2d46d64af0c11694cff4daf1afdd0
Matthew Atkinson <M47791@icloud.com>
2021-08-05 14:41:22 -0400

    initial commit

---
+++ Transportly.App/Program.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
+using Serilog.Events;
+using System.Threading.Tasks;
+using Transportly.Commands;
+using Transportly.Infrastructure;
+
+namespace Transportly.App
+{
+    class Program
+    {
+        public static async Task<int> Main(string[] args)
+        {
+            return await CommandParser.Parse(BuildHost().Services, args);
+        }
+
+        private static IHost BuildHost()
+        {
+            return Host.CreateDefaultBuilder()
+                .UseSerilog((_, config) =>
+                {
+                    config.MinimumLevel.Override("Microsoft", LogEventLevel.Error)
+                          .MinimumLevel.Override("Microsoft.Hosting.Lifetime", LogEventLevel.Error)
+                          .WriteTo.Console(
+                            outputTemplate: "[{Level:u3}] {Message:lj}{NewLine}{Exception}");
+                })
+                .ConfigureServices(services =>
+                {
+                    services.AddLogging();
+                    services.AddInfrastructure();
+                    services.AddCliCommands();
+                })
+                .Build();
+        }
+    }
+}
